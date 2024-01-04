c89f0f5991e9a0154f1b6166cff9e368e8717d0f
Yuehan He <122114676+mperform@users.noreply.github.com>
2024-01-04 15:45:39 -0500

    almost done the create post

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -7,20 +7,22 @@ const Bottombar = () => {
       {bottombarLinks.map((link) => {
         const isActive = pathname === link.route;
         return (
-          <li
-            className={`leftsidebar-link group ${isActive && "bg-primary-500"}`}
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}
           >
-            <Link to={link.route} className="flex gap-4 items-center p-4">
-              <img
-                src={link.imgURL}
-                alt={link.label}
-                className={`group-hover:invert-white ${
-                  isActive && "invert-white"
-                }`}
-              />
-              {link.label}
-            </Link>
-          </li>
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`${
+                isActive && "invert-white"
+              }`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
         );
       })}
     </section>
